5ed26e833c7c51446ae5d971ea06a0c13819476a
Paul <cureu.paul.ioan@gmail.com>
2024-11-06 21:37:24 +0200

    Test CanHaveAnExponent

--- Json.Start/Json.Facts/JsonNumberFacts.cs
+++ Json.Start/Json.Facts/JsonNumberFacts.cs
@@ -91,7 +91,7 @@ namespace Json.Facts
             Assert.False(IsJsonNumber("12.3x"));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void CanHaveAnExponent()
         {
             Assert.True(IsJsonNumber("12e3"));
