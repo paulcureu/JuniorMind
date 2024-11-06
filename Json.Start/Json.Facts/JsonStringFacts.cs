6e9fae3ae588e1b9094657e469ddd9753abe7227
Paul <cureu.paul.ioan@gmail.com>
2024-11-06 18:09:12 +0200

    Test CanContainEscapedLineFeed

--- Json.Start/Json.Facts/JsonStringFacts.cs
+++ Json.Start/Json.Facts/JsonStringFacts.cs
@@ -83,7 +83,7 @@ namespace Json.Facts
             Assert.True(IsJsonString(Quoted(@"a \f b")));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void CanContainEscapedLineFeed()
         {
             Assert.True(IsJsonString(Quoted(@"a \n b")));
