23884b51c2ff4d218f67ca482e9bef02b659e2d8
Paul <cureu.paul.ioan@gmail.com>
2024-11-06 19:11:49 +0200

    Test DoesNotContainUnrecognizedExcapceCharacters and ContainsExcapeCharacter

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -6,7 +6,7 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
-            return HasContent(input) && IsDoubleQuoted(input) && !ContainsControlCharacters(input);
+            return HasContent(input) && IsDoubleQuoted(input) && !ContainsControlCharacters(input) && ContainsExcapeCharacter(input);
         }
 
         private static bool IsDoubleQuoted(string input)
@@ -24,6 +24,41 @@ namespace Json
             foreach (char character in input)
             {
                 if (char.IsControl(character))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool ContainsExcapeCharacter(string input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == '\\' && !char.IsLetter(input[i + 1]))
+                {
+                    return true;
+                }
+                else if (input[i] == '\\' && IsExcapeCharacter(input[i + 1]))
+                {
+                    return true;
+                }
+                else if (input[i] == '\\' && !IsExcapeCharacter(input[i + 1]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsExcapeCharacter(char character)
+        {
+            const string excapeCharacter = @"\\[^ntrbfv\u""]";
+            for (int i = 0; i < excapeCharacter.Length; i++)
+            {
+                if (character == excapeCharacter[i])
                 {
                     return true;
                 }
